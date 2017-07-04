f16977213b0654646d87e72aac21c6f5bdddaffd
xu <zhikuixu@gmail.com>
2017-07-03 22:59:49 -0700

    supporting macros and calls

--- parsing.py
+++ parsing.py
@@ -1,20 +1,25 @@
 def parse(command):
     args = command.split(' ')
     cmd = args[0]
-    rargs = [-1, -1, -1, -1]
-    rargs[1] = int(args[1])
-    rargs[2] = -1
-    rargs[3] = -1
-    if len(args) > 2:
-        rargs[2] = int(args[2])
-    if len(args) > 3:
-        rargs[3] = int(args[3])
-    return cmd, rargs[1], rargs[2], rargs[3]
+    if cmd != '#':
+        init = 1
+    else:
+        return cmd, args
+    for i in range(init, len(args)):
+        args[i] = int(args[i])
+    return cmd, args
 class command:
     def __init__(self, content):
         self.content = content
-        self.args = [-1, -1, -1, -1]
-        self.cmd, self.args[1], self.args[2], self.args[3] = parse(content)
-        self.args[0] = self.cmd
+        self.cmd, self.args = parse(content)
     def display(self):
         print(self.content)
+    def max_reg(self):
+        if self.cmd == 's' or self.cmd == 'z':
+            return self.args[1]
+        elif self.cmd == 'j' or self.cmd == 'asn':
+            return max(self.args[1], self.args[2])
+        elif self.cmd == '#':
+            return 0
+        else:
+            return max(self.args[1:])
--- reg_machine.py
+++ reg_machine.py
@@ -1,35 +1,75 @@
 import parsing
 import sys
-max = 0
-commands = []
-file = open(sys.argv[1], 'r')
-for line in file:
-    cmd = parsing.command(line)
-    commands.append(cmd)
-    for arg in cmd.args[1:]:
-        if arg > max:
-            max = arg
-tape = [0 for i in range(max)]
-for i in range(2, len(sys.argv)):
-    tape[i-1] = int(sys.argv[i])
-k = 0
-count = 0
-MAXCOUNT = 100000000
-inf = sys.maxsize
-while k < len(commands):
-    count = count + 1
-    if count >= MAXCOUNT:
-        tape[0] = inf
-        break
-    command = commands[k]
-    if command.cmd == 'z':
-        tape[command.args[1]] = 0
-    elif command.cmd == 'j':
-        if tape[command.args[1]] == tape[command.args[2]]:
-            k = command.args[3]
-            continue
-    elif command.cmd == 's':
-        tape[command.args[1]] = tape[command.args[1]] + 1
-    k = k + 1
+register_machines = {}
+number_of_spaces = 0
+def display(msg):
+    for i in range(number_of_spaces):
+        print('    ', end='')
+    print(msg)
+class SIZE_LIMITS:
+    MAX_REG_ALLOWED = 100
+    MAX_RUN_TIME_ALLOWED = 100000000
+class rm(SIZE_LIMITS):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.max_reg = 0
+        self.commands = []
+        register_machines[file_name] = self
+        file = open(self.file_name, 'r')
+        for line in file:
+            cmd = parsing.command(line)
+            if cmd.cmd == '#':
+                new_rm = rm(cmd.args[1])
+                continue
+            self.commands.append(cmd)
+            if cmd.max_reg() > self.max_reg:
+                self.max_reg = cmd.max_reg()
+        if self.max_reg > self.MAX_REG_ALLOWED:
+            print('number of register used (' + self.max_reg +') is over the limit (' + self.MAX_REG_ALLOWED + ')')
+            exit(0)
+    def run(self, inputs):
+        if self.max_reg < len(inputs):
+            self.max_reg = len(inputs)
+        self.tape = [0 for i in range(self.max_reg + 1)]
+        for i in range(len(inputs)):
+            self.tape[i+1] = int(inputs[i])
+        display(self.file_name + ' begin')
+        self.count = 0
+        self.cmd_index = 0
+        while self.cmd_index < len(self.commands):
+            display(self.tape)
+            self.count = self.count + 1
+            if self.count >= self.MAX_RUN_TIME_ALLOWED:
+                self.tape[0] = float('Inf')
+                break
+            current_command = self.commands[self.cmd_index]
+            if current_command.cmd == 'z':
+                self.tape[current_command.args[1]] = 0
+            elif current_command.cmd == 'j':
+                if self.tape[current_command.args[1]] == self.tape[current_command.args[2]]:
+                    self.cmd_index = current_command.args[3]
+                    continue
+            elif current_command.cmd == 's':
+                self.tape[current_command.args[1]] = self.tape[current_command.args[1]] + 1
+            elif current_command.cmd == 'asn':
+                self.tape[current_command.args[1]] = self.tape[current_command.args[2]]
+            elif current_command.cmd in register_machines:
+                args = []
+                for arg in current_command.args[2:]:
+                    args.append(self.tape[arg])
+                global number_of_spaces
+                number_of_spaces = number_of_spaces + 1
+                self.tape[current_command.args[1]] = register_machines[current_command.cmd].run(args)
+                number_of_spaces = number_of_spaces - 1
+            else:
+                print('unrecognized command: ' + current_command.cmd)
+                exit(0)
+            self.cmd_index = self.cmd_index + 1
+        display(self.tape)
+        display(self.file_name + ' end')
+        return self.tape[0]
 
-print(tape[0])
+main = rm(sys.argv[1])
+result = main.run(sys.argv[2:])
+print(result)
+exit(0)
